9df62a6faa7ffba4762f8cfbd0a5bf2a2576d2ae
AbhijeetBiswal <abhijeetbiswal.code@gmail.com>
2023-10-13 20:54:19 +0530

    initial commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <div className="bg-neutral-900 h-full w-full overflow-hidden overflow-y-auto rounded-lg">
+      <Header>
+        Header
+      </Header>
+    </div>
+  );
+}
